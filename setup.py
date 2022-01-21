e1efba7c671ac743c4b5ce1d1780d496fc635171
ludel <ludovic.delsol@ynov.com>
2022-01-21 12:20:03 +0100

    Rename package

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 from setuptools import setup, find_packages
 
-from admin_views import __version__
+from custom_admin_views import __version__
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     README = readme_file.read()
 
 setup(
     author='Ludovic Delsol',
     author_email='ludel47@gmail.com',
-    classifiers=['Development Status :: 1 - Production/Stable',
-                 'Environment :: Web Environment',
-                 'Framework :: Django',
-                 'Intended Audience :: Developers',
-                 'License :: OSI Approved :: Apache Software License',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python :: 3',
-                 'Topic :: Utilities'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities'
+    ],
     description='Create custom view for Django admin.',
     include_package_data=True,
     install_requires=['django'],
     license='APL',
     long_description=README,
-    name='django-admin-views',
+    name='django-admin-custom-views',
     packages=find_packages(exclude=['sample_project']),
-    url='https://github.com/iambrandontaylor/django-admin-sortable',
+    url='https://github.com/ludel/django-admin-views',
     version=__version__,
-    zip_safe=False
 )
